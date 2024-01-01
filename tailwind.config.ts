52f3ad4c85ba1c01406b6f58a380f6c4029325ff
Rafael Thumaza <rassraza4@gmail.com>
2024-01-01 09:18:40 -0300

    initial config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        Orange: "hsl(26, 100%, 55%)",
+        "Pale-orange": "hsl(25, 100%, 94%)",
+        "Very-dark-blue": " hsl(220, 13%, 13%)",
+        "Dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "Grayish-blue": "hsl(220, 14%, 75%)",
+        "Light-grayish-blue": "hsl(223, 64%, 98%)",
+        White: "hsl(0, 0%, 100%)",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
